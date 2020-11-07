8dbd0942b0ddcc67185c35961cf62b1f725804b3
Aidy <microsoft@aidanjenkins.co.uk>
2020-11-07 18:16:31 +0000

    Added integration test

--- spec/integration-spec.ts
+++ spec/integration-spec.ts
@@ -1,7 +1,16 @@
+import { Row } from "../src/Row";
 import { Table } from "../src/Table";
 
 describe("Integration", () => {
     const table = new Table("MyTable");
+
+    it("Should be able to store a column and row of data and allow me to retrieve it", () => {
+        table.columns.add("Foo");
+        const row = new Row({table, model: { Foo: "bar" }});
+        table.rows.push(row);
+
+        expect(table.rows[0].field("Foo")).toBe("bar");
+    });
 
 
 });
